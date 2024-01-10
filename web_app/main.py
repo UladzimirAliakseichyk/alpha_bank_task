e8768ddb45af19e8cee1d9120a65c2956852c73b
UladzimirAliakseuchyk <145700536+UladzimirAliakseichyk@users.noreply.github.com>
2024-01-10 11:37:19 +0300

    Update main.py

--- web_app/main.py
+++ web_app/main.py
@@ -14,11 +14,3 @@ app.include_router(
 
 app.include_router(auth_router)
 
-user_depencency = Annotated[dict, Depends(get_current_user)]
-
-@app.get('/get_user', status_code=status.HTTP_200_OK)
-async def user(user: user_depencency):
-    if user is None:
-        print(user)
-        raise HTTPException(status_code=401, detail='Authentication Faild')
-    return {'User': user['username']}
